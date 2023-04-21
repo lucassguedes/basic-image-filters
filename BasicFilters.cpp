91335c277f65dcd9a59c1c0d568efe5e472a858d
Lucas Guedes <lucasguedes778@gmail.com>
2023-04-21 16:13:03 -0300

    Teste inicial, gerando imagem invertida.

--- BasicFilters.cpp
+++ BasicFilters.cpp
@@ -20,8 +20,23 @@ int main(int argc, char ** argv)
         return -1;
     }
 
+    for(int i = 0; i < image.rows; i++)
+    {
+        for(int j = 0; j < image.cols; j++)
+        {
+            Vec3b cor = image.at<Vec3b>(i, j);
+
+            cor[0] = 255 - cor[0];
+            cor[1] = 255 - cor[1];
+            cor[2] = 255 - cor[2];
+
+            image.at<Vec3b>(i, j) = cor;
+        }
+    }
+
     namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", image);
+    imwrite("finlandia-invertida.jpg", image);
     waitKey(0);
     
     return 0;
