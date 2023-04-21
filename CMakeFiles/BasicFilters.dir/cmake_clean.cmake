844014809cb0bf49540fea8a22069779735890fa
Lucas Guedes <lucasguedes778@gmail.com>
2023-04-21 15:57:47 -0300

    First commit.

---
+++ CMakeFiles/BasicFilters.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BasicFilters"
+  "BasicFilters.pdb"
+  "CMakeFiles/BasicFilters.dir/BasicFilters.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicFilters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
